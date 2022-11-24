ff2773ffd79a70752d257e077a00812686b31aa7
Felipe Kühne <fkuhne@gmail.com>
2022-11-24 14:23:33 +0000

    wip

--- 01_04b/components/Card.js
+++ 01_04b/components/Card.js
@@ -4,23 +4,28 @@
  * - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
  */
 
-const buildImage = (imgData) => {
-  let srcset = `${imgData.urls.full} ${imgData.width}w`;
-  if (imgData.urls.regular) {
-    srcset = srcset + `, ${imgData.urls.regular} 1080w`;
+const buildImage = ({
+  description,
+  width,
+  height,
+  urls: { full, regular, small },
+}) => {
+  let srcset = `${full} ${width}w`;
+  if (regular) {
+    srcset = srcset + `, ${regular} 1080w`;
   }
-  if (imgData.urls.small) {
-    srcset = srcset + `, ${imgData.urls.small} 400w`;
+  if (small) {
+    srcset = srcset + `, ${small} 400w`;
   }
 
   const img = `
     <img
           srcset="${srcset}"
           sizes="(max-width: 450px) 400px, (max-width: 800) 1080px"
-          src="${imgData.urls.regular}"
-          width="${imgData.width}"
-          height="${imgData.height}"
-          alt="${imgData.description}"
+          src="${regular}"
+          width="${width}"
+          height="${height}"
+          alt="${description}"
           loading="lazy"
         />
     `;
@@ -28,7 +33,8 @@ const buildImage = (imgData) => {
 };
 
 const getDate = (imgData) => {
-  const date = new Date(imgData.created_at);
+  const { created_at: createdDate } = imgData;
+  const date = new Date(createdDate);
   const niceDate = date.toLocaleString("default", {
     year: "numeric",
     month: "long",
@@ -37,24 +43,34 @@ const getDate = (imgData) => {
   return niceDate;
 };
 
+// De-structuring the JSON data in order to improve efficiency,
+//  so we don't have to traverse the JSON tree every time we need
+//  to access some data.
 const Card = (imgData) => {
+  const {
+    description,
+    user: { name },
+    created_at: createdDate,
+    links: { self },
+  } = imgData;
+
   return `
     <figure class="image">
       ${buildImage(imgData)}
       <figcaption class="image__caption">
-        <h3 class="image__title">${imgData.description}</h3>
+        <h3 class="image__title">${description}</h3>
         <div class="image__meta">
           <p>
             Photo by
-            <span class="image__photog">${imgData.user.name}</span>.
+            <span class="image__photog">${name}</span>.
           </p>
           <p>
-            Uploaded on <time class="image__date" datetime="${
-              imgData.created_at
-            }">${getDate(imgData)}</time>.
+            Uploaded on <time class="image__date" datetime="${createdDate}">${getDate(
+    imgData
+  )}</time>.
           </p>
           <p>
-            <a href="${imgData.links.self}" class="image__link">
+            <a href="${self}" class="image__link">
               View it on Unsplash.
             </a>
           </p>
